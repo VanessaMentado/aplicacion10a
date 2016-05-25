6e6408c42b7bcf9876d8874feb4918386e315b29
VanessaMentado <vanessadsaupoo@gmail.com>
2016-05-25 08:55:57 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,5 +1,5 @@
 //JavaScript
-$(document).ready(function(e) {
+$(document).ready(function (e) {
     $('#principal').height($('#page1').height());
 	document.addEventListener("deviceready",function(){
 		
@@ -15,7 +15,7 @@ $(document).ready(function(e) {
 		 
 		 $('.nota').bind('touchstart',function(){
 			 $(this).addClass('tocada');
-			 audio.plays($(this).attr('id'));
+			 audio.play($(this).attr('id'));
 			 
 		 }).bind('touchend',function(){
 			 $(this).removeClass('tocada');
